548564f2abdae788c96d5e030814087d43524714
aymane8414 <aymanearbai@gmail.com>
2025-10-17 15:54:53 +0200

    ajout fonction similarité cos dans la classe

--- backend/vectorisation.py
+++ backend/vectorisation.py
@@ -1,8 +1,7 @@
 from gensim.models import Word2Vec
 from nltk.tokenize import word_tokenize
-import nltk
+import nltk, numpy as np
 
-# Si pas déjà téléchargé
 nltk.download('punkt')
 
 class VectoriseurWord2Vec:
@@ -21,6 +20,19 @@ class VectoriseurWord2Vec:
                 vects.append(self.model.wv[mot])
         if not vects:
             return [0.0] * self.taille
-        # moyenne
+        # moyenne des vecterus
         s = sum(vects)
         return s / len(vects)
+
+
+    def similarite_cosine(v1: np.ndarray, v2: np.ndarray) -> float:
+        """cos(v1, v2), [0..1] """
+        num = float(np.dot(v1, v2))
+        den = float(np.linalg.norm(v1) * np.linalg.norm(v2))
+        return (num / den) if den > 0 else 0.0
+
+    def similarite_entre_phrases(self, p1: str, p2: str) -> float:
+        """Cosinus entre deux phrases, moyenne des mots"""
+        v1 = self.vectoriser_phrase(p1)
+        v2 = self.vectoriser_phrase(p2)
+        return self.similarite_cosine(v1, v2)
